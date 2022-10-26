3028a3a48a182a6b797de2ff7506a71d37bc72e8
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-10-26 20:50:40 +0300

    first helloworld rest api created

---
+++ src/main/java/com/kamilisler/rest/websevices/restfullwebservices/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.kamilisler.rest.websevices.restfullwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    //@RequestMapping(method = RequestMethod.GET,path = "/hello-world") annotation
+    // gereksiz import kaldırma : ctrl+shift+o
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello Restfull web services training!222";
+    }
+}
