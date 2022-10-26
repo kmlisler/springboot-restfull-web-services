25efab5a9f50f9cdfae97519deaa18c2f435c623
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-10-26 20:36:59 +0300

    project created with initializer

---
+++ src/test/java/com/kamilisler/rest/websevices/restfullwebservices/RestfullWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamilisler.rest.websevices.restfullwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
