05a81e8002434a29db3f9c5e255cacf40b4e899c
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-10-30 22:10:25 +0300

    static filtering and dynamic filtering implemented

---
+++ src/main/java/com/kamilisler/rest/websevices/restfullwebservices/filtering/FilteringController.java
@@ -0,0 +1,31 @@
+package com.kamilisler.rest.websevices.restfullwebservices.filtering;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+    @GetMapping("/filtering")
+    public MappingJacksonValue retrieveSomeBean(){
+        // field1,field3 retrieve
+
+        SomeBean bean = new SomeBean("value1","value2","value3");
+
+        MappingJacksonValue mapping = new MappingJacksonValue(bean);
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field3");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter",filter);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+
+
+
+}
